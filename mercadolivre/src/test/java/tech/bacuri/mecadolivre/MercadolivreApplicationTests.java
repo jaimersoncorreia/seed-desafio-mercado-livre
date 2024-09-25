892c0a7fd73e0721f3e8b464602ad83ca37246b1
Jaimerson <jaimersonsantos@ufam.edu.br>
2024-09-25 19:59:27 -0400

    estrutura do projeto de treino mercadolivre

---
+++ mercadolivre/src/test/java/tech/bacuri/mecadolivre/MercadolivreApplicationTests.java
@@ -0,0 +1,13 @@
+package tech.bacuri.mecadolivre;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MercadolivreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
