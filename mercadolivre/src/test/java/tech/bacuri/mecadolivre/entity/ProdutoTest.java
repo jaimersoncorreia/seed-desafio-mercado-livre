582efd08daa26f1970cd68cada8a4ebeafc565db
Jaimerson <jaimersonsantos@ufam.edu.br>
2024-10-07 18:24:37 -0400

    desafio 2 - finaliza compra - parte 1 - teste parte 1

--- mercadolivre/src/test/java/tech/bacuri/mecadolivre/entity/ProdutoTest.java
+++ mercadolivre/src/test/java/tech/bacuri/mecadolivre/entity/ProdutoTest.java
@@ -4,6 +4,7 @@ import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.CsvSource;
 import org.junit.jupiter.params.provider.MethodSource;
 import tech.bacuri.mecadolivre.dto.NovaCaracteristicaForm;
 import tech.bacuri.mecadolivre.dto.SenhaLimpa;
@@ -61,5 +62,39 @@ class ProdutoTest {
                         new NovaCaracteristicaForm("key4", "value4")
                 ))
         );
+    }
+
+    @DisplayName("não aceita abater estoque <= zero")
+    @ParameterizedTest
+    @CsvSource({"0", "-1", "-100"})
+    void abataEstoque1(Integer estoque) {
+        var caracteristicas = List.of(
+                new NovaCaracteristicaForm("key", "value"),
+                new NovaCaracteristicaForm("key2", "value2"),
+                new NovaCaracteristicaForm("key3", "value3")
+        );
+        var categoria = new Categoria("categoria");
+        var dono = new Usuario("email@email.com.br", new SenhaLimpa("senhaaa"));
+        var produto = new Produto("nome", BigDecimal.TEN, 10L, "descrição", categoria, dono, caracteristicas);
+
+        Assertions.assertThrows(IllegalArgumentException.class, () -> produto.abataEstoque(estoque));
+    }
+
+    @DisplayName("verifica estoque do produto")
+    @ParameterizedTest
+    @CsvSource({"1,1,true", "1,2,false", "4,2,true", "1,5,false"})
+    void abataEstoque2(Long qtdeEstoque, Integer qtdePedido, Boolean esperado) {
+        var caracteristicas = List.of(
+                new NovaCaracteristicaForm("key", "value"),
+                new NovaCaracteristicaForm("key2", "value2"),
+                new NovaCaracteristicaForm("key3", "value3")
+        );
+        var categoria = new Categoria("categoria");
+        var dono = new Usuario("email@email.com.br", new SenhaLimpa("senhaaa"));
+        var produto = new Produto("nome", BigDecimal.TEN, qtdeEstoque, "descrição", categoria, dono, caracteristicas);
+
+        boolean resultado = produto.abataEstoque(qtdePedido);
+
+        Assertions.assertEquals(esperado, resultado);
     }
 }
